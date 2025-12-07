072135a9ad10968abeb6ff46c007a0438fc3c21e
Orlex69 <alexis.brad@ynov.com>
2025-12-07 18:53:47 +0100

    création builder monstres

--- app/src/main/java/projet/Class/Character/AventurerFactory.java
+++ app/src/main/java/projet/Class/Character/AventurerFactory.java
@@ -1,18 +1,18 @@
 package projet.Class.Character;
 
 public class AventurerFactory {
-    
-    public static Aventurer CreatAventurer(int choice){
+
+    public static Aventurer CreatAventurer(int choice) {
         switch (choice) {
             case 1:
-               return new Human.Builder();
-            
+                return new Human();
+
             case 2:
                 return new Elf();
-            
+
             case 3:
                 return new Dwarf();
-        
+
             default:
                 throw new IllegalArgumentException("erreur vous n'avez pas choisi un aventurier");
         }
--- app/src/main/java/projet/Class/Character/FactoryMonstre.java
+++ app/src/main/java/projet/Class/Character/FactoryMonstre.java
@@ -5,13 +5,49 @@ public class FactoryMonstre {
     public static Monster createMonster(String type) {
         switch (type.toLowerCase()) {
             case "undead":
-                return new Undead();
+                return createUndead();
             case "orc":
-                return new Orc();
+                return createOrc();
             case "tyranide":
-                return new Tyranide();
+                return createTyranide();
             default:
                 throw new IllegalArgumentException("Unknown monster type: " + type);
         }
+    }
+
+    public static Orc createOrc() {
+        return new Orc.OrcBuilder()
+                .life(120)
+                .speed(30)
+                .armor(40)
+                .attack(50)
+                .xpValue(20)
+                .level(5)
+                .height(2.5)
+                .build();
+    }
+
+    public static Undead createUndead() {
+        return new Undead.UndeadBuilder()
+                .life(65)
+                .speed(35)
+                .armor(20)
+                .attack(20)
+                .xpValue(25)
+                .level(3)
+                .resurractionRate(0.5)
+                .build();
+    }
+
+    public static Tyranide createTyranide() {
+        return new Tyranide.TyranideBuilder()
+                .life(10)
+                .speed(30)
+                .armor(20)
+                .attack(20)
+                .xpValue(20)
+                .level(2)
+                .claws(2)
+                .build();
     }
 }
