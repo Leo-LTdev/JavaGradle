6137f1e887dba9d192b7a7f2fa0a89db3165518a
Orlex69 <alexis.brad@ynov.com>
2025-12-08 12:28:32 +0100

    escape method

--- app/src/main/java/projet/Class/Character/Aventurer.java
+++ app/src/main/java/projet/Class/Character/Aventurer.java
@@ -5,14 +5,14 @@ import projet.Class.Object.Gear;
 import projet.Class.Object.Weapon;
 import projet.Class.Object.Armor;
 
-
 public class Aventurer extends Character {
+
     private ArrayList<Object> inventory;
     private Gear gear;
     private int level;
     private int exp;
 
-    public Aventurer(){
+    public Aventurer() {
         super();
         this.level = 1;
         this.exp = 0;
@@ -20,32 +20,32 @@ public class Aventurer extends Character {
         this.gear = new Gear();
     }
 
-    public Aventurer(int life, int speed, int armor, int attack,Gear gear, int level, int exp){
-        super(life,speed,armor,attack);
+    public Aventurer(int life, int speed, int armor, int attack, Gear gear, int level, int exp) {
+        super(life, speed, armor, attack);
         this.gear = gear;
         this.level = level;
         this.exp = exp;
     }
 
     //All get
-    public ArrayList<Object> getInventory(){
+    public ArrayList<Object> getInventory() {
         return inventory;
     }
 
-    public Gear getGear(){
+    public Gear getGear() {
         return gear;
     }
 
-    public int getLevel(){
+    public int getLevel() {
         return level;
     }
 
-    public int getExp(){
+    public int getExp() {
         return exp;
     }
 
     //All set
-    protected void setIventory(Object object){
+    protected void setIventory(Object object) {
         inventory.add(object);
     }
 
@@ -58,20 +58,26 @@ public class Aventurer extends Character {
         System.out.println("EXP : " + getExp());
     }
 
-    protected boolean isDead(){
+    protected boolean isDead() {
         if (getLife() <= 0) {
             return true;
         }
         return false;
     }
 
-    protected void equipWeapon(Weapon weapon){
+    protected void equipWeapon(Weapon weapon) {
         this.gear.setWeapon(weapon);
     }
 
-    protected void equipArmor(Armor armor){
+    protected void equipArmor(Armor armor) {
         this.gear.setArmor(armor);
     }
 
-
+    protected void escape() {
+        if (Math.random() < 0.3) {
+            System.out.println("Vous avez réussi à fuir le combat !");
+        } else {
+            System.out.println("Échec de la fuite ! Vous allez affronter la réalité big noob !");
+        }
+    }
 }
