85feb5b0eb70b1a40036eccdaebbd328a8e8ce94
Orlex69 <alexis.brad@ynov.com>
2025-12-08 09:43:19 +0100

    add random values to builder

--- app/src/main/java/projet/Class/Character/FactoryMonstre.java
+++ app/src/main/java/projet/Class/Character/FactoryMonstre.java
@@ -1,6 +1,18 @@
 package projet.Class.Character;
 
+import java.util.Random;
+
 public class FactoryMonstre {
+
+    private static final Random RANDOM = new Random();
+
+    private static int randomInt(int min, int max) {
+        return min + RANDOM.nextInt(max - min + 1);
+    }
+
+    private static double randomDouble(double min, double max) {
+        return min + RANDOM.nextDouble() * (max - min);
+    }
 
     public static Monster createMonster(String type) {
         switch (type.toLowerCase()) {
@@ -17,37 +29,37 @@ public class FactoryMonstre {
 
     public static Orc createOrc() {
         return new Orc.OrcBuilder()
-                .life(120)
-                .speed(30)
-                .armor(40)
-                .attack(50)
-                .xpValue(20)
-                .level(5)
-                .height(2.5)
+                .life(randomInt(100, 150))
+                .speed(randomInt(20, 40))
+                .armor(randomInt(30, 50))
+                .attack(randomInt(40, 60))
+                .xpValue(randomInt(15, 30))
+                .level(randomInt(4, 7))
+                .height(randomDouble(2.0, 3.0))
                 .build();
     }
 
     public static Undead createUndead() {
         return new Undead.UndeadBuilder()
-                .life(65)
-                .speed(35)
-                .armor(20)
-                .attack(20)
-                .xpValue(25)
-                .level(3)
-                .resurractionRate(0.5)
+                .life(randomInt(50, 80))
+                .speed(randomInt(25, 45))
+                .armor(randomInt(10, 30))
+                .attack(randomInt(15, 30))
+                .xpValue(randomInt(20, 35))
+                .level(randomInt(2, 5))
+                .resurractionRate(randomDouble(0.3, 0.7))
                 .build();
     }
 
     public static Tyranide createTyranide() {
         return new Tyranide.TyranideBuilder()
-                .life(10)
-                .speed(30)
-                .armor(20)
-                .attack(20)
-                .xpValue(20)
-                .level(2)
-                .claws(2)
+                .life(randomInt(20, 40))
+                .speed(randomInt(25, 40))
+                .armor(randomInt(5, 15))
+                .attack(randomInt(15, 30))
+                .xpValue(randomInt(15, 30))
+                .level(randomInt(1, 4))
+                .claws(randomInt(2, 4))
                 .build();
     }
 }
