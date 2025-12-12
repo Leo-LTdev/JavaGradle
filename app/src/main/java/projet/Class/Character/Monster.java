0cfd9eb5b2bdd75ceb9f5ba7bee7159e247fa278
Orlex69 <alexis.brad@ynov.com>
2025-12-12 16:47:40 +0100

    update dealDamage monstre

--- app/src/main/java/projet/Class/Character/Monster.java
+++ app/src/main/java/projet/Class/Character/Monster.java
@@ -1,9 +1,9 @@
 package projet.Class.Character;
 
 public abstract class Monster extends Character {
+
     private int xpValue;
     private int level;
-
 
     public Monster() {
         super();
@@ -12,13 +12,27 @@ public abstract class Monster extends Character {
     }
 
     public Monster(int life, int speed, int armor, int attack, int xpValue, int level) {
-        super(life,speed,armor,attack);
+        super(life, speed, armor, attack);
         this.xpValue = xpValue;
         this.level = level;
     }
-    
 
-    public int getXpvalue(){
+    public void dealDamage(Character target) {
+        int damage = this.getAttack() - damageReduction(target);
+        if (damage < 0) {
+            damage = 0;
+        }
+        target.setLife(target.getLife() - damage);
+
+        if (target.isDead()) {
+            System.out.println("Le monstre vous a tué : " + damage);
+        } else {
+            System.out.println("Le monstre vous a infligé " + damage + " dégats");
+            System.out.println("Il vous reste " + target.getLife() + " PV");
+        }
+    }
+
+    public int getXpvalue() {
         return xpValue;
     }
 }
